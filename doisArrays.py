8366f0f3011aadd7f14ed720cf3ab4e65fc44cdc
Leonardo Morais <69097237+leodemorais@users.noreply.github.com>
2023-10-09 18:57:05 -0300

    array nome_idade

---
+++ doisArrays.py
@@ -0,0 +1,12 @@
+# Array de produtos de uma floricultura
+produtos_flori = ["Azaléia", "Begônia", "Crisântemo", "Fertilizante", "Terra Vegetal"]
+
+# Array de anos de nascimento da família
+anos_nascimento = [1946, 1955, 1975, 1977, 1985]
+
+# Imprimir os arrays no terminal
+print("Produtos da floricultura: ")
+print(produtos_flori)
+
+print("\nAnos de nascimento da família:")
+print(anos_nascimento)
