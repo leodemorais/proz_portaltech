94d7220afe24fecf7de7d6c6e8331629a7d3b938
Leonardo Morais <69097237+leodemorais@users.noreply.github.com>
2023-10-09 19:27:50 -0300

    array lista_musicos

---
+++ listaMusicos.py
@@ -0,0 +1,15 @@
+#Copie o array apresentado embaixo no seu editor de código,
+# e imprima no terminal: a quantidade de elementos que ele possui,
+# o dado salvo no índice 2, o dado salvo no índice 9, e dado salvo no índice 14.
+
+lista_musicos = [ 'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque',
+                  'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola',
+                  'Yamandú Costa', 'Gal Costa']
+
+qnt_musicos = len(lista_musicos)
+print("A quantidade total de músicos na lista é de", qnt_musicos)
+print("O músico escolhido é", lista_musicos[2])
+print("O músico escolhido é", lista_musicos[9])
+print("O músico escolhido é", lista_musicos[14])
+
+
